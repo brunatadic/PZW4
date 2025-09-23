0b9a920db828bba2f16838d1f8a62be98d9f7ff7
brunatadic <150249670+brunatadic@users.noreply.github.com>
2025-09-23 09:18:49 +0200

    Add files via upload

---
+++ skripta-2.js
@@ -0,0 +1,22 @@
+//varijable
+let ime = "Ivo";
+let prezime = "Ivić";
+let a = 2;
+let b = 5;
+let c = a + b;
+
+//konstante 
+const pi = 3.14;
+const oib = 12345678991;
+
+//ispis varijabli
+console.log(ime);
+console.log(prezime);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(pi);
+console.log(oib);
+
+//Ispis na web stranici
+document.getElementById("ispis"),innerHTML = ime;
